dddc0fa2edc9b475a4c0d6131063819e200d814d
Denis <dhoornae@ulb.ac.be>
2018-04-23 22:41:41 +0200

    File renaming

---
+++ suggestedMain.py
@@ -0,0 +1,16 @@
+import sys
+from pythonInjector.src.generator import Generator
+from pythonInjector.src.lexer.error import UnclosedEnvironmentError
+
+extension = "inj"
+
+if (__name__ == '__main__'):
+    try:
+        Generator.generates(sys.argv[1]) # file path
+    except IndexError as e:
+        print("Haven't you forgot to specify a '."+extension+"' file ?")
+    except UnclosedEnvironmentError as e:
+        print("Are you sure that you have closed all your python environments ?")
+    except Exception as e:
+        print("Well... Something really unexpected happened.")
+        print("Hint : "+str(e))
